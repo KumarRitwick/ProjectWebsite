fcc1e8f97dfd04955ef8e818f1faeb555d033e50
Ritwick <kr.ritwick@gmail.com>
2025-01-08 15:21:23 +0000

    Button Alignment Fix Added

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -9,8 +9,8 @@ export default function HomePage() {
                 <p>Hi, before we start let me introduce myself, I am</p>
                 <h1 className='uppercase font-semibold text-4xl sm:text-5xl md:text-6xl lg:text-7xl'>Ritwick <span className='text-purple-400'>Kumar</span></h1>
             </div>
-            <p className='text-sm md:text-base font-light'>I hereby acknowledgement that these projects have come out of pure <span className='text-purple-400 font-medium'>love for coding and learning new technology.</span> The projects in here are something that I did while learning <span className='text-purple-400 font-medium'>a specific technology.</span> Please feel free to browse and have a look, if you have any sugeestions, do drop me an email.</p>
-            <div className='flex flex-col gap-4'>
+            <p className='text-sm md:text-base font-light'>I hereby acknowledgement that these projects have come out of pure <span className='text-purple-400 font-medium'>love for coding, learning new technology & sheer boredom.</span> The projects in here are something that I did while learning <span className='text-purple-400 font-medium'>a specific technology.</span> Please feel free to browse and have a look, if you have any suggestion, do drop me an email.</p>
+            <div className='flex flex-row gap-4'>
                 <Button func={() => {
                     window.location.href = '#projects'
                 }} text={"Projects"} className='custom-text'></Button>
